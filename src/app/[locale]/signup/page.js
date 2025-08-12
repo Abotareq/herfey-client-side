ae10a2a2835fbe15d0228a5ba8bdce37c6bd4eeb
mohammed6699 <mohammedahmed.refaat@gmail.com>
2025-08-12 18:12:26 +0300

    handle the localization and the style of signup and signin

---
+++ src/app/[locale]/signup/page.js
@@ -0,0 +1,9 @@
+
+import Signup from '../components/SignUp';
+export const metadata = {
+  title: "SignUp",
+  description: "Sign up page"
+}
+export default function SignUp() {
+  return <Signup />
+}
