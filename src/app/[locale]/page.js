0f3996fbb3f2c40b0e8cbd491fbc81d1a4c0b36d
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2025-08-12 15:33:44 +0300

    sing in - sing up pages

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import SignIn from "./singin/page.js";
+import SignUp from "./signup/pages.js";
 export default function Home() {
   return (
     <>
-      <Header />
-      <Footer />
+  {/*     <Header /> */}
+     <SignUp />
+ {/*      <Footer /> */}
     </>
   );
 }
