22c1ac60b244588ae2da86f4cf711bf1d0d8b31e
mohammed6699 <mohammedahmed.refaat@gmail.com>
2025-08-22 17:03:45 +0300

    product service

--- src/service/productService.js
+++ src/service/productService.js
@@ -1,22 +1,51 @@
-import axios from 'axios';
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE,
-  withCredentials: true,
+const BASE_URL = "http://localhost:5000/api";
 
-});
+// axios to fetch 
+const ProductAPI = axios.create({
+    baseURL: BASE_URL,
+    headers:{
+        "Content-Type": "application/json"
+    },
+    // cookies
+    withCredentials: true
+})
 
+// helper function Get All
+const getAllProducts = async(params = {}) => {
+    const response = await ProductAPI.get('/product', {params});
+    return response.data
+}
+// helper function to get product by Id
+const getProductById = async(productId) => {
+    const response = await ProductAPI.get(`/product/${productId}`);
+    return response.data
+}
 
-export const createProductAPI = async (formData) => {
-
-  try {
-    const response = await apiClient.post('/product', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-    return response.data;
-  } catch (error) {
-    throw new Error(error.response?.data?.message || 'An unexpected error occurred');
-  }
-};
+// get all products
+export const useGetAllProducts = (params = {}) => {
+    return useQuery({
+        queryKey: ['product', params],
+        queryFn: () => getAllProducts(params),
+        onError: (error) => {
+            console.error("Failed to get All Products",
+                error.response?.data?.message || error.message
+            )
+        }
+    })
+}
+// get product by Id
+export const useGetProductById = (productId) => {
+    return useQuery({
+        queryKey: ['product', productId],
+        queryFn: () => getProductById(productId),
+        enabled: !!productId,
+        onError: (error) => {
+            console.error("failed to get product by id",
+                error.response?.data?.message || error.message
+            )
+        }
+    })
+}
