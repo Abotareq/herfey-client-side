b85efd3dd57a79ffcded2a6b5c4cedb4d2d94edb
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2025-08-25 16:15:43 +0300

    updates

--- src/app/[locale]/components/Breadcrumbs.jsx
+++ src/app/[locale]/components/Breadcrumbs.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { usePathname } from "@/i18n/navigation";
 import { useGetProductById } from "@/service/product";
@@ -17,39 +17,42 @@ function Breadcrumbs() {
   // pathSegments = pathSegments.filter(
   //   (seg) => !/^\d+$/.test(seg) && !/^[a-f0-9]{10,}$/i.test(seg) // adjust regex to your case
   // );
-  const entityType = pathSegments[0] // store and product ids
+  const entityType = pathSegments[0]; // store and product ids
   const entityId = pathSegments.length > 1 ? pathSegments[1] : null;
 
   // fetch the product details
-  const {data: productRes} = useGetProductById(entityType === "products" ? entityId : null, {
-    enabled: entityType === "products" && !!entityId,
-  })
+  const { data: productRes } = useGetProductById(
+    entityType === "products" ? entityId : null,
+    {
+      enabled: entityType === "products" && !!entityId,
+    }
+  );
   // fetch the store details
   const { data: storeRes } = useStore(
-  entityType === "store" ? entityId : null,
-  entityType === "store" && !!entityId 
-);
-  
+    entityType === "store" ? entityId : null,
+    entityType === "store" && !!entityId
+  );
+
   const product = productRes?.data || null;
   const store = storeRes || null;
-  console.log(`store data: ${store}`)
+  console.log(`store data: ${store}`);
   const breadcrumbs = pathSegments.map((segment, index) => {
     const href = "/" + pathSegments.slice(0, index + 1).join("/");
     let label;
-    if(entityType === "products" && segment === entityId && product){
-      label = product.name
-    }else if(entityType === "store" && segment === entityId && store){
-      label = store.name
-    }else{
+    if (entityType === "products" && segment === entityId && product) {
+      label = product.name;
+    } else if (entityType === "store" && segment === entityId && store) {
+      label = store.name;
+    } else {
       label = t(segment, {
         default: segment.charAt(0).toUpperCase() + segment.slice(1),
-      })
+      });
     }
     return { href, label };
   });
 
   return (
-    <nav className="mx- text-sm text-gray-600 my-4">
+    <nav className="mx-10 text-sm text-gray-600 my-4">
       <ol className="flex items-center space-x-2">
         <li>
           <Link href="/" className="text-orange-600 hover:underline">
