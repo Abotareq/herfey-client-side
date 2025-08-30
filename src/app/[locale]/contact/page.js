0a34fa2c87b3374eb379fdf3cd8a4c9571f4cd0d
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2025-08-30 22:11:48 +0300

    //

--- src/app/[locale]/contact/page.js
+++ src/app/[locale]/contact/page.js
@@ -142,7 +142,7 @@ export default function ContactPage() {
               </div>
             </div>
 
-            <input
+            <textarea
               type="text"
               className={`w-full h-12 text-gray-600 placeholder-gray-400 bg-transparent text-lg shadow-sm font-normal leading-7 rounded-full border border-gray-200 focus:outline-none pl-4 mb-10 ${
                 isArabic ? "pr-[10px]" : ""
